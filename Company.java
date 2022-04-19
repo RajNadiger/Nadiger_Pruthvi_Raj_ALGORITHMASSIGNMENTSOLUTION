63a3a342573eb2fca228e9605b63f6bf92ab4328
Nadiger Pruthvi Raj Srinivas <pruthvirajnadiger@gmail.com>
2022-04-19 12:55:49 +0900

    Create Company.java

---
+++ Company.java
@@ -0,0 +1,22 @@
+package com.assignment2.model;
+
+public class Stock {
+    private double stockPrice;
+    private boolean stockVariation;
+
+    public double getStockPrice() {
+        return stockPrice;
+    }
+
+    public void setStockPrice(double stockPrice) {
+        this.stockPrice = stockPrice;
+    }
+
+    public boolean isStockVariation() {
+        return stockVariation;
+    }
+
+    public void setStockVariation(boolean stockVariation) {
+        this.stockVariation = stockVariation;
+    }
+}
